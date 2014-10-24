f9d652dd4d969f8a05bd4996eb95b4f0fd59625c
Steve Skutnik <skutnik@gmail.com>
2014-10-24 12:27:23 -0400

    Added LOGGER messages (hello world/nurse) to Tick() & Tock()

--- src/tutorial_storage_facility.cc
+++ src/tutorial_storage_facility.cc
@@ -11,10 +11,17 @@ std::string Tutorial_storageFacility::str() {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void Tutorial_storageFacility::Tick() {}
+void Tutorial_storageFacility::Tick() {
+
+   LOG(cyclus::LEV_INFO1,"tutorial_storage") << "Hello...";
+
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void Tutorial_storageFacility::Tock() {}
+void Tutorial_storageFacility::Tock() {
+
+   LOG(cyclus::LEV_INFO1,"tutorial_storage") << "...nurse!";
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 extern "C" cyclus::Agent* ConstructTutorial_storageFacility(cyclus::Context* ctx) {
